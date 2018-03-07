91b8f49cb413adce7e085edcf5eaa90c6ebd1561
Dan <Salazar@nyu.edu>
2018-03-07 15:25:39 -0800

    made some sorting edits and added a function that run all three

--- get_followers.py
+++ get_followers.py
@@ -24,9 +24,10 @@ def df_maker(df):
             name=number.name.encode('utf-8')
             screen_name=number.screen_name
             description=number.description.encode('utf-8')
+            location=number.location.encode('utf-8')
             followers=number.followers_count
             following=number.friends_count
-            favorites=number.favourites_count
+            #favorites=number.favourites_count
             protected=number.protected
             verified=number.verified
             status=number.statuses_count
@@ -35,27 +36,40 @@ def df_maker(df):
                 last_tweet=str(number.status.created_at.month)+'/'+str(number.status.created_at.year)
                 active_years=(number.status.created_at.year)-(number.created_at.year)
                 tweets_per_year=status/active_years
+                retweeted=number.status.retweet_count
+                favorited=number.status.favorite_count
             except:
-                last_tweet=str('while_ago_or_never')
+                last_tweet=str('N/A')
                 active_years=str('N/A')
                 tweets_per_year=str('N/A')
+                retweeted=str('N/A')
+                favorited=str('N/A')
             try:
                 ratio=(following/float(followers))
             except:
                 ratio=str('N/A')
                 
-            records.append((name, screen_name, description, followers, following, favorites,
-                            protected, verified, status, last_tweet, created, active_years, tweets_per_year, ratio))
-    complete_df = pd.DataFrame.from_records(records, columns=['Name', 'Screen Name', 'Description', 
-                                                              'Followers', 'Following', 'Favorite Count', 'Private?', 
-                                                              'Verified?', 'Number of Tweets', 'Last Tweet', 
-                                                              'Year Created', 'Active Years', 'Avg Tweets Per Year', 
-                                                              'Following/Followers Ratio'])
+            records.append((name, screen_name, description, location, followers, following,
+                            protected, verified, status, last_tweet, retweeted, favorited, created, 
+                            active_years, tweets_per_year, ratio))
+    complete_df = pd.DataFrame.from_records(records, columns=['Name', 'Screen Name', 'Description', 'Location',
+                                                              'Followers', 'Following', 'Private?', 
+                                                              'Verified?', 'Number of Tweets', 'Lastest Tweet Date', 
+                                                              'Lastest Tweet Retweets ', 'Lastest Tweet Favorites', 
+                                                              'Year Account Created', 'Active Years', 
+                                                              'Avg Tweets Per Active Year', 'Following/Followers Ratio'])
     return complete_df
 
 def sorter(df, follower_cutoff):
     sorted_df=df.loc[df['Followers'] > follower_cutoff].sort_values(['Verified?', 'Following/Followers Ratio',
-                                                                     'Followers', 'Active Years'], 
-                                                                    ascending=[False, True, False, False])
+                                                                     'Followers'], 
+                                                                    ascending=[False, True, False])
 
     return sorted_df
+
+def all_together_now(handle, num):
+    followers = get_followers(handle)
+    made = df_maker(followers)
+    sort = sorter(made,num)
+    return sort
+    
